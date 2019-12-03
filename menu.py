9ced3f52faa7fb88d8004231bd4a5135f1c4d319
P4trick-p <3527184+P4trick-p@users.noreply.github.com>
2019-12-03 16:26:00 +0100

    upd

---
+++ menu.py
@@ -0,0 +1,27 @@
+ans=True
+while ans:
+    print ("""
+    1.Algorytm 1
+    2.Algorytm 2
+    3.Algorytm 3
+    4.Algorytm 4
+    5.Wyjdz
+    """)
+    ans=raw_input("Wybierz opcje: ")
+    if ans=="1":
+      print("\n Uruchamianie algorytmu 1")
+
+    elif ans=="2":
+      print("\n Uruchamianie algorytmu 2")
+
+    elif ans=="3":
+      print("\n Uruchamianie algorytmu 3")
+
+    elif ans=="4":
+      print("\n Uruchamianie algorytmu 4")
+
+    elif ans=="5":
+      print("\n Do widzenia")
+
+    elif ans !="":
+      print("\n Nieprawidłowy argument")
